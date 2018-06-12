c5ed42c8770071429d588b034020757ed031e8f2
newbuxcoin <39801527+newbuxcoin@users.noreply.github.com>
2018-06-12 18:35:05 +0530

    coin
    coin

--- src/main.h
+++ src/main.h
@@ -28,7 +28,7 @@ class CNode;
 
 class CTxMemPool;
 
-static const int LAST_POW_BLOCK = 450;
+static const int LAST_POW_BLOCK = 470;
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
